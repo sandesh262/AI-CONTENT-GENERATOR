5c8177891879c657f672e15f2012a5f3c801a964
rahul kumbhar <rakumbhar5278@gmail.com>
2025-01-28 23:28:39 +0530

    .

--- drizzle.config.js
+++ drizzle.config.js
@@ -5,6 +5,6 @@ export default {
     schema: "./utils/schema.tsx",
     dialect: 'postgresql',
     dbCredentials: {
-      url:"postgresql://neondb_owner:daiFxr0p9MeO@ep-jolly-cell-a9rjnvuw.gwc.azure.neon.tech/ai-content-generator?sslmode=require"
+      url:process.env.NEXT_PUBLIC_DRIZZLE_DB_URL
     }
   };
